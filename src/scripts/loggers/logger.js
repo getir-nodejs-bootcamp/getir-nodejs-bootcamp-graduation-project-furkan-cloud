41d4a3ce89e2541aa66e64149bbc041782464f61
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-27 18:28:27 +0300

    igitgnore file updated for log files

--- src/scripts/loggers/logger.js
+++ src/scripts/loggers/logger.js
@@ -2,6 +2,6 @@ const path = require("path")
 const fs = require('fs')
 
 // create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, "../../", "/logs", 'access.log'), { flags: 'a' })
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "../../", "logs", 'access.log'), { flags: 'a' })
 
 module.exports = accessLogStream
