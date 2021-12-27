f66f23f44b833ef06787a347896ea4fff3a05dc3
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-27 18:29:39 +0300

    validation for records object updated

--- src/validations/Records.js
+++ src/validations/Records.js
@@ -1,11 +1,11 @@
 const Joi = require('joi').extend(require('@joi/date'));
 
-// Validation for get request for records 
+// Validation for get request for records
 const getRecords = Joi.object({
   startDate: Joi.date().format('YYYY-MM-DD').utc().required(),
-  endDate: Joi.date().required(),
+  endDate: Joi.date().format('YYYY-MM-DD').utc().required().min(Joi.ref('startDate')),
   minCount: Joi.number().required().min(0),
-  maxCount: Joi.number().required().positive(),
+  maxCount: Joi.number().required().positive().min(Joi.ref('minCount'))
 });
 
 module.exports = {
