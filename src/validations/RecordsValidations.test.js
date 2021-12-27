c16d801a4a41731eae460474bdf1880030b45112
furkan-cloud <firstinfirstout2020@gmail.com>
2021-12-28 00:57:26 +0300

    comments added for tests and error handler

--- src/validations/RecordsValidations.test.js
+++ src/validations/RecordsValidations.test.js
@@ -3,12 +3,15 @@ const Mongoose = require('mongoose');
 const app = require('../config/app');
 
 describe('Test the validations for /records url', () => {
+  // change default max time for testing
   jest.setTimeout(15000);
 
+  // connect db to test validations before testing
   beforeAll(() => {
     Mongoose.connect(process.env.MONGO_URI);
   });
 
+  // disconnect from db after all tests done
   afterAll((done) => {
     Mongoose.disconnect(done);
   });
